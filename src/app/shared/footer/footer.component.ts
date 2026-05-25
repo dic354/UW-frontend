5b75dc93110857c537b3bb26063f0d39fe3b6a9f
dic354 <davidigco7299@gmail.com>
2026-05-25 13:10:55 +0200

    footer y navbar ajustes menores

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, // Para usar el async pipe con el estado del usuario
+    RouterLink    // Para habilitar el uso de routerLink en el HTML
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  
+  // Inyectamos el servicio Auth
+  authService = inject(AuthService);
 
+  // Compartimos el flujo del usuario con el HTML usando el pipe async
+  usuario$ = this.authService.usuario$;
 }
